2cbf73179a9c0bb17efc6094ab839e4594355faa
monk <monk@bignerdranch.com>
2013-08-05 15:35:12 -0500

    Restores Crypto compatibility for pre-7.0 OS X

--- BNRPersistence/BNRCrypto.m
+++ BNRPersistence/BNRCrypto.m
@@ -5,9 +5,13 @@
 //  Created by Adam Preble on 4/8/10.
 //  Copyright 2010 Big Nerd Ranch. All rights reserved.
 //
+#import <ConditionalMacros.h>
+#import <AvailabilityMacros.h>
 
 #import "BNRCrypto.h"
 
+#if TARGET_OS_IPHONE || (TARGET_OS_MAC && (MAC_OS_X_VERSION_MAX_ALLOWED > 1070)) 
+// Note that macros such as MAC_OS_X_VERSION_10_7 don't exist in earlier SDKs; Apple recommends using the numeric value
 
 #import <Security/SecRandom.h>
 
@@ -16,13 +20,13 @@ void BNRRandomBytes(void *buffer, int length)
     SecRandomCopyBytes(kSecRandomDefault, length, (uint8_t*)buffer);
 }
 
-//#else // Mac:
-//
-//#import <openssl/rand.h>
-//#import <libkern/OSTypes.h>
-//
-//void BNRRandomBytes(void *buffer, int length)
-//{
-//    RAND_pseudo_bytes((UInt8*)buffer, length);
-//}
-//#endif
+#else // Older OS X
+barf
+#import <openssl/rand.h>
+#import <libkern/OSTypes.h>
+
+void BNRRandomBytes(void *buffer, int length)
+{
+    RAND_pseudo_bytes((UInt8*)buffer, length);
+}
+#endif
